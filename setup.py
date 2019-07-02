2fa6deb2a6f749f6ef841a25183ced991547052a
Uilian Ries <uilianries@gmail.com>
2019-07-02 13:13:32 -0300

    Fix setup
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_version():
                                             "bintray", "bintray.py"))
     with open(filename, "rt") as version_file:
         conan_init = version_file.read()
-        version = re.search("__version__ = '([0-9a-z.-]+)'", conan_init).group(1)
+        version = re.search('__version__ = "([0-9a-z.-]+)"', conan_init).group(1)
         return version
 
 setup(
