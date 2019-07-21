b76d09545bc00387d9719a9c4ca7cd8697959bf8
Uilian Ries <uilianries@gmail.com>
2019-07-20 22:35:30 -0300

    #19 Update entitlement
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- tests/test_entitlements.py
+++ tests/test_entitlements.py
@@ -24,19 +24,21 @@ def test_create_entitlement():
     bintray = Bintray()
     error_message = ""
     try:
-        bintray.create_entitlement("uilianries", "foobar", "generic", "statistics", "test")
+        bintray.create_entitlement("uilianries", "generic", "statistics", "test", "rw",
+                                   ["key1", "key2"], "a/b/c", ["tag1", "tag2"])
     except Exception as error:
         error_message = str(error)
 
-    assert "Could not GET (403): This resource is only available for subjects with entitlement " \
+    assert "Could not POST (403): This resource is only available for subjects with entitlement " \
            "management." == error_message
 
     try:
-        bintray.create_entitlement("jfrog", "foobar", product="xray")
+        bintray.create_entitlement("jfrog", access="rw", access_keys=["key1", "key2"], path="a/b/c",
+                                   tags=["tag1", "tag2"], product="xray")
     except Exception as error:
         error_message = str(error)
 
-    assert "Could not GET (403): forbidden" == error_message
+    assert "Could not POST (403): forbidden" == error_message
 
 
 def test_delete_entitlement():
@@ -58,3 +60,22 @@ def test_delete_entitlement():
     assert "Could not DELETE (403): forbidden" == error_message
 
 
+def test_update_entitlement():
+    bintray = Bintray()
+    error_message = ""
+    try:
+        bintray.update_entitlement("uilianries", "foobar", "generic", "statistics", "test", "rw",
+                                   ["key1", "key2"], ["tag1", "tag2"])
+    except Exception as error:
+        error_message = str(error)
+
+    assert "Could not PATCH (403): This resource is only available for subjects with entitlement " \
+           "management." == error_message
+
+    try:
+        bintray.update_entitlement("jfrog", "foobar", access="rw", access_keys=["key1", "key2"],
+                                   tags=["tag1", "tag2"], product="xray")
+    except Exception as error:
+        error_message = str(error)
+
+    assert "Could not PATCH (403): forbidden" == error_message
