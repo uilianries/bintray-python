cba43641251569db3b35654f73ff9ceb5a4180c7
Uilian Ries <uilianries@gmail.com>
2019-07-21 00:11:55 -0300

    Update docs
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,6 @@ release = '0.8.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'rinoh.frontend.sphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
